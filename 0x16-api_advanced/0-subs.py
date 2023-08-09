d5b9731fbfdaca34f62871886e20a7d2a21a8f8b
briankmbugua <briankmbuguak@gmail.com>
2023-08-09 11:19:15 +0300

    number of subsribers for a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-Agent": "python:number_of_subscribers (by u/briankinyanjui)"
+    }
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+
+        return 0
+
+
+if __name__ == "__main__":
+    print(number_of_subscribers(sys.argv[1]))
